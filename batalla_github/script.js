da62d510c8f4946e8ee818b16a8061777af13a0a
Matias Bensan A <mbensan@gmail.com>
2022-07-05 10:31:39 -0400

    Ejemplo de Batalla de Github con Clases ES6

--- batalla_github/script.js
+++ batalla_github/script.js
@@ -12,6 +12,12 @@ const total_1 = document.querySelector('.card-user-1 .total')
 const input2 = document.querySelector('#username2')
 const btn2 = document.querySelector('#btn2')
 
+const public_repos_2 = document.querySelector('.card-user-2 .public_repos')
+const followers_2 = document.querySelector('.card-user-2 .followers')
+const following_2 = document.querySelector('.card-user-2 .following')
+const public_gists_2 = document.querySelector('.card-user-2 .public_gists')
+const total_2 = document.querySelector('.card-user-2 .total')
+
 btn1.addEventListener('click', async function () {
   const username1 = input1.value.trim();
 
@@ -22,11 +28,54 @@ btn1.addEventListener('click', async function () {
   const datos = await fetch(`https://api.github.com/users/${username1}`)
   const user = await datos.json()
 
-  console.log(user);
   public_repos_1.textContent = user.public_repos
   public_gists_1.textContent = user.public_gists
   followers_1.textContent = user.followers
   following_1.textContent = user.following
 
   total_1.textContent = user.public_gists + user.public_repos + user.followers + user.following
+})
+
+btn2.addEventListener('click', async function () {
+  const username2 = input2.value.trim();
+
+  if (username2 == '') {
+    return;
+  }
+
+  const datos = await fetch(`https://api.github.com/users/${username2}`)
+  const user = await datos.json()
+
+  public_repos_2.textContent = user.public_repos
+  public_gists_2.textContent = user.public_gists
+  followers_2.textContent = user.followers
+  following_2.textContent = user.following
+
+  total_2.textContent = user.public_gists + user.public_repos + user.followers + user.following
+})
+
+document.getElementById('btn-battle').addEventListener('click', function () {
+  
+  if (total_1.textContent == '' || total_2.textContent == '') {
+    return;
+  }
+  console.log('msj');
+
+  let msj = ''
+  const total_num1 = parseInt(total_1.textContent)
+  const total_num2 = parseInt(total_2.textContent)
+  
+  if (total_num1 > total_num2) {
+    msj = 'Gana el usuario 1'
+  } else if (total_num1 < total_num2) {
+    msj = 'Gana el usuario 2'
+  } else {
+    msj = 'Hay un empate!!!'
+  }
+
+  Swal.fire(
+    'Resultado de la Batalla',
+    msj,
+    'success'
+  )
 })
---
+++ batalla_github/script_clases.js
@@ -0,0 +1,94 @@
+class Usuario {
+  // "card_name" es la clase de la 'card' que contiene todo el resto de cosas 
+  constructor (card_name) {
+    //this.put_html(card_name)
+    
+    // etiquetas HTML del usuario
+    this.input = document.querySelector(`.${card_name} .username`)
+    this.btn_buscar = document.querySelector(`.${card_name} .btn-buscar`)
+
+    this.public_repos = document.querySelector(`.${card_name} .public_repos`)
+    this.followers = document.querySelector(`.${card_name} .followers`)
+    this.following = document.querySelector(`.${card_name} .following`)
+    this.public_gists = document.querySelector(`.${card_name} .public_gists`)
+    this.total = document.querySelector(`.${card_name} .total`)
+
+    this.btn_buscar.addEventListener('click',  this.get_score)
+  }
+
+  get_score = async () => {
+    const username = this.input.value.trim();
+
+    if (username == '') {
+      return;
+    }
+
+    const datos = await fetch(`https://api.github.com/users/${username}`)
+    const user = await datos.json()
+
+    this.public_repos.textContent = user.public_repos
+    this.public_gists.textContent = user.public_gists
+    this.followers.textContent = user.followers
+    this.following.textContent = user.following
+
+    this.total.textContent = user.public_gists + user.public_repos + user.followers + user.following
+  }
+
+  put_html (card_name) {
+    const row_users = document.querySelector('.users')
+    row_users.innerHTML += `
+      <div class="col-6">
+        <div class="card ${card_name}">
+          <div class="card-body">
+            <h5 class="card-title">Jugador 1</h5>
+            <div class="row">
+              <div class="col-9">
+                <input type="text" class="form-control username">
+              </div>
+              <div class="col-3">
+                <button class="btn btn-primary btn-buscar">GO!</button>
+              </div>
+            </div>
+          </div>
+          <div class="card-body">
+            <ul class="list-group">
+              <li class="list-group-item">Public Repos <span class="public_repos"></span></li>
+              <li class="list-group-item">Followers <span class="followers"></span></li>
+              <li class="list-group-item">Following <span class="following"></span></li>
+              <li class="list-group-item">Gists <span class="public_gists"></span></li>
+              <li class="list-group-item"><b>Total: <span class="total"></span></b></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    `
+  } 
+}
+
+const user1 = new Usuario('card-user-1')
+const user2 = new Usuario('card-user-2')
+
+document.getElementById('btn-battle').addEventListener('click', function () {
+  
+  if (user1.total.textContent == '' || user2.total.textContent == '') {
+    return;
+  }
+
+  let msj = ''
+  const total_num1 = parseInt(user1.total.textContent)
+  const total_num2 = parseInt(user2.total.textContent)
+  
+  if (total_num1 > total_num2) {
+    msj = 'Gana el usuario 1'
+  } else if (total_num1 < total_num2) {
+    msj = 'Gana el usuario 2'
+  } else {
+    msj = 'Hay un empate!!!'
+  }
+
+  Swal.fire(
+    'Resultado de la Batalla',
+    msj,
+    'success'
+  )
+})
