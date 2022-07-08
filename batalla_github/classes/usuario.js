23f1ce8dbd0bc87448d0062223b2de27c685d27b
Matias Bensan A <mbensan@gmail.com>
2022-07-08 13:00:59 -0400

    FIX: Uso de clases en Batalla de Github

---
+++ batalla_github/classes/usuario.js
@@ -0,0 +1,85 @@
+export class Usuario {
+  // "card_name" es la clase de la 'card' que contiene todo el resto de cosas 
+  constructor (card_name) {
+    //this.put_html(card_name)
+    
+    // etiquetas HTML del usuario
+    this.input = document.querySelector(`.${card_name} .username`)
+    this.btn_buscar = document.querySelector(`.${card_name} .btn-buscar`)
+
+    this.public_repos = document.querySelector(`.${card_name} .public_repos`)
+    this.followers = document.querySelector(`.${card_name} .followers`)
+    this.following = document.querySelector(`.${card_name} .following`)
+    this.public_gists = document.querySelector(`.${card_name} .public_gists`)
+    this.total = document.querySelector(`.${card_name} .total`)
+
+    this.avatar = document.querySelector(`.${card_name} img`)
+
+    // guardo el card_name como propiedad del objeto
+    this.card_name = card_name
+
+    // eventos
+    this.btn_buscar.addEventListener('click',  this.get_score)
+
+    // carga inicial
+    if (localStorage.getItem(card_name)) {
+      this.input.value = localStorage.getItem(card_name)
+    }
+  }
+
+  get_score = async () => {
+    const username = this.input.value.trim();
+
+    if (username == '') {
+      return;
+    }
+    // consulto datos del usuario a la API
+    const datos = await fetch(`https://api.github.com/users/${username}`)
+    const user = await datos.json()
+
+    // cargo avatar del usuario
+    this.avatar.src = user.avatar_url;
+    this.avatar.style.display = 'block'
+
+    // Cargo puntajes del usuario
+    this.public_repos.textContent = user.public_repos
+    this.public_gists.textContent = user.public_gists
+    this.followers.textContent = user.followers
+    this.following.textContent = user.following
+
+    this.total.textContent = user.public_gists + user.public_repos + user.followers + user.following
+    
+    // guardo el usuario buscado en localStorage
+    localStorage.setItem(this.card_name, username)
+  }
+
+  put_html (card_name) {
+    const row_users = document.querySelector('.users')
+    row_users.innerHTML += `
+      <div class="col-6">
+        <div class="card ${card_name}">
+          <div class="card-body">
+            <h5 class="card-title">Jugador 1</h5>
+            <div class="row">
+              <div class="col-9">
+                <input type="text" class="form-control username">
+              </div>
+              <div class="col-3">
+                <button class="btn btn-primary btn-buscar">GO!</button>
+              </div>
+            </div>
+          </div>
+          <div class="card-body">
+            <ul class="list-group">
+              <li class="list-group-item">Public Repos <span class="public_repos"></span></li>
+              <li class="list-group-item">Followers <span class="followers"></span></li>
+              <li class="list-group-item">Following <span class="following"></span></li>
+              <li class="list-group-item">Gists <span class="public_gists"></span></li>
+              <li class="list-group-item"><b>Total: <span class="total"></span></b></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    `
+  } 
+}
